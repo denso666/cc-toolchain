33c847bea5b9df70c57167afa741452f9b746060
RodMe27 <rodrigo.ormed@gmail.com>
2023-04-27 18:16:12 -0400

    Make directory command created

---
+++ src/mkdir/mkdir.c
@@ -0,0 +1,18 @@
+#include "mkdir.h"
+
+int MakeDir_Function(char * directory)
+{
+    int ret_val = 0;
+    int val = mkdir(directory, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (val ==  0)
+    {
+        printf("Directory created\n");
+    }
+    else
+    {
+        ret_val = -1;
+        fprintf(stderr, "Error: %s\n", strerror(errno));
+        return ret_val;
+    }
+    return ret_val;
+}
