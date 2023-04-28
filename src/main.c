12e8aa34eb89b9520dfcc3e0e2aa247625658f19
Fairbrook <kevinvr@hotmail.es>
2023-04-27 19:11:24 -0600

    added ls to menu

--- src/main.c
+++ src/main.c
@@ -1,37 +1,53 @@
+#include "ls/ls.h"
+#include <limits.h>
 #include <stdio.h>
+#include <string.h>
 
 int menu(void);
 
-int main()
-{
-    int opt = menu();
-
-    if (opt == 1)
-    {
-        printf("You select '1'\n");
-    }
-    else
-    {
-        printf("Command not found :(\n");
-    }
-
-    return 0;
+void clean_buffer() {
+  while (getchar() != '\n');
 }
 
-int menu(void)
-{
-    printf("CC-TOOLCHAIN\n");
-    printf("1. command_1\n");
-    printf("2. command_2\n");
-    printf("3. command_3\n");
-    printf("n. command_n\n");
-    printf("? ");
-    
-    char in = getchar();
-    if (in != EOF)
-    {
-        in -= '0';
-        if (in >= 0 && in <= 9) return (int) in;
+int main() {
+  int opt = menu();
+
+  if (opt == 1) {
+    printf("Directorio a listar > ");
+    char path[PATH_MAX];
+    char *read_path = fgets(path, PATH_MAX, stdin);
+    if (read_path != NULL) {
+      int len = strlen(read_path);
+      read_path[len - 1] = '\0';
     }
+    if(ls(path) == EXIT_FAILURE){
+        perror("ls error");
+        return EXIT_FAILURE;
+    }
+  }
+
+  if (opt == -1) {
+    printf("Command not found :(\n");
     return -1;
+  }
+
+  return 0;
+}
+
+int menu(void) {
+  printf("CC-TOOLCHAIN\n");
+  printf("1. ls\n");
+  printf("2. cp\n");
+  printf("3. chown\n");
+  printf("n. command_n\n");
+  printf("? ");
+
+  char in = getchar();
+  clean_buffer();
+  if (in != EOF) {
+    in -= '0';
+    if (in >= 0 && in <= 9)
+      return (int)in;
+  }
+  return -1;
 }
